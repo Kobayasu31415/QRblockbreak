d05851e24455c3731d4e8634c42484db4dfb39bf
Kobayasu31415 <hurinux@gmail.com>
2023-03-11 19:16:39 +0900

    ver1完成

--- Assets/script/Block.cs
+++ Assets/script/Block.cs
@@ -3,16 +3,30 @@ using DG.Tweening;
 
 class Block : MonoBehaviour, IBreakable
 {
+    float m_deltaTime;
+
     public void OnBreak()
     {
-        Destroy(gameObject);
+        transform.DOScale(Vector3.zero, 0.2f).SetEase(Ease.InOutQuart).OnComplete(() => Destroy(gameObject));
+        //transform.DOScale(Vector3.zero, 0.2f).SetEase(Ease.).OnComplete(()=> Destroy(gameObject));
     }
 
-     void OnCollisionEnter(Collision collision) 
+    void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Ball"))
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ball"))
         {
             OnBreak();
-        }            
+        }
+    }
+
+    void Update()
+    {
+        m_deltaTime += Time.deltaTime;
+
+        if (m_deltaTime > 2)
+        {
+            transform.DOScale(0.2f, 0.2f).SetLoops(2, LoopType.Yoyo);
+            m_deltaTime = 0f;
+        }
     }
 }
